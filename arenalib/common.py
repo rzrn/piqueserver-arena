35e3fb71fe99dbaf83d3ced8e38cdae0d0fc255d
rzrn <rzrngh@outlook.com>
2026-02-12 14:56:21 +0700

    arenalib/common.py: update

--- arenalib/common.py
+++ arenalib/common.py
@@ -1,4 +1,4 @@
-# Copyright © 2024–2025 rzrn
+# Copyright © 2024–2026 rzrn
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
